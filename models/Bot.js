138a973daf16e6c1d2c46ac6c15ee6a1f692cc17
sasiyamd3-cell <sasiyamd3@gmail.com>
2026-09-18 02:56:49 -0700

    Update Bot.js

--- models/Bot.js
+++ models/Bot.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const botSchema = new mongoose.Schema({
   botId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   phoneNumber: { type: String, default: null },
-  status: { type: String, default: 'offline' }, // offline | connecting | online
+  status: { type: String, default: 'offline' },
   stats: {
     rss: { type: String, default: '0M' },
     heap: { type: String, default: '0M/0M' },
